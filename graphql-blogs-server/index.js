b05b780bff3bdba22d31420e68788445b8037018
Zoef <shaikhzoef36@gmail.com>
2024-05-04 17:51:31 +0530

    add resolvers

--- graphql-blogs-server/index.js
+++ graphql-blogs-server/index.js
@@ -1,8 +1,29 @@
 import { ApolloServer, gql } from 'apollo-server';
+import { MongoClient } from 'mongodb'
+const MONGO_URI = "mongodb://127.0.0.1:27017/blogs";
+
+let dbInstance = null;
+async function connectToCluster() {
+  if (dbInstance) {
+    return dbInstance;
+  }
+  const client = new MongoClient(MONGO_URI);
+  try {
+    console.log("Connecting to the server...");
+    await client.connect();
+    console.log("Connected successfully to server");
+    dbInstance = client.db("blogs");
+    return dbInstance;
+  } catch (err) {
+    console.error(err);
+    throw err;
+  }
+}
+connectToCluster(MONGO_URI);
 
 const typeDefs = gql`
 	type Article {
-		id: Int!
+		_id: String!
 		title: String
 		content: String
 		createdAt: String
@@ -24,10 +45,26 @@ const typeDefs = gql`
 	}
 `;
 
-const resolvers = {};
+const resolvers = {
+  Query: {
+    articles: async () => {
+      const db = await connectToCluster();
+      const articles = await db?.collection("blogs")?.find().toArray();
+      return articles;
+    }
+  },
+  Mutation: {
+    article: async (_, { title, content }) => {
+      const db = await connectToCluster();
+      const article = await db?.collection("blogs")?.insertOne({ title, content, createdAt: new Date() })
+      console.log(article)
+      return article;
+    }
+  }
+};
 
 const server = new ApolloServer({ typeDefs, resolvers });
 
 server.listen({ port: 4000 }).then(({ url }) => {
-	console.log(`this port ${url}`);
+  console.log(`this port ${url}`);
 });
